2bc5fb949576bcf3bb6e5e92ffa09d790bf6074f
Mike Prokopchuk <m.prokopchuk@office.ngs.ru>
2014-09-11 13:47:54 +0700

    v2.0

--- runcmd.go
+++ runcmd.go
@@ -197,6 +197,7 @@ func NewRemoteRunner(user, host, key string) (*Remote, error) {
 		User: user,
 		Auth: []ssh.AuthMethod{ssh.PublicKeys(signer)},
 	}
+
 	server, err := ssh.Dial("tcp", host, config)
 	if err != nil {
 		return &Remote{}, err
